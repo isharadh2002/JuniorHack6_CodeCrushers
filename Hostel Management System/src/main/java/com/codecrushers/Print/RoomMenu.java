2de17ca971d78a30ef13c69fd7edec2bc65c6617
Ishara Dhanushan <109136246+isharadh2002@users.noreply.github.com>
2025-01-25 12:15:29 +0530

    Update RoomMenu.java

--- Hostel Management System/src/main/java/com/codecrushers/Print/RoomMenu.java
+++ Hostel Management System/src/main/java/com/codecrushers/Print/RoomMenu.java
@@ -1,11 +1,11 @@
 package com.codecrushers.Print;
 
+import com.codecrushers.CrudOPR.RoomOPR;
+
+import java.util.Scanner;
+
 public class RoomMenu {
-
-
-
-
-
+    Scanner scanner = new Scanner(System.in);
 
     public void printMenu(){
         System.out.println("Rooms Managements.../n/n ");
@@ -15,5 +15,11 @@ public class RoomMenu {
         System.out.println("[4] Delete Rooms");
         System.out.println("[0] Back to Previous");
 
+        int choice = scanner.nextInt();
+        RoomOPR roomOPR = new RoomOPR();
+
+        if(choice == 1){
+            
+        }
     }
 }
